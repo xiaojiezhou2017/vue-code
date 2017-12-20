4935570a721af5307b0acc11d32627a1ac11b30c
xiaojiezhou <zhouxiaojie@hualala.com>
2017-12-20 16:30:22 +0000

    双向绑定例子完成

--- example/observer/index.js
+++ example/observer/index.js
@@ -6,37 +6,100 @@ class Observer {
     walk (obj) {
         const keys = Object.keys(obj);
         for (let i = 0; i < keys.length; i ++ ) {
-            this.observer(obj,keys[i],obj[keys[i]])
+            this.observer.call(this,obj,keys[i],obj[keys[i]])
         }
     }
     observer (obj, key, value) {
+        const dep = new Dep();
         Object.defineProperty(obj, key, {
             get () {
+                dep.depend();
                 return value;
             },
-            set: function (v) {
-                render(v);
-                return v;
+            set (v) {
+                value = v;
+                dep.notify();
             }
         }) 
     }
 }
+
+const stack = [];
+
+const pushStack = (watcher) => {
+    Dep.target = watcher;
+    stack.push(watcher);
+}
+const popStack = () => {
+    Dep.target = stack.pop()
+}
+
+
+
+let DepId = 0;
+
+class Dep {
+    constructor () {
+        this.watchers = [];
+        this.id = DepId ++;
+    }
+    pushWatch (watch) {
+        this.watchers.push(watch);
+    }
+    depend () {
+        Dep.target.addDep(this);
+    }
+    notify () {
+        this.watchers.forEach(w => {
+            w.update();
+         })
+    }
+}
+
+class Watcher {
+    constructor (render,ctx) {
+        this.render = render;
+        this.ctx = ctx;
+        this.deps = [];
+        this.get();
+    }
+　　 addDep (dep){
+        const deps = this.deps;
+        const record = deps.find(i => i.id === dep.id);
+        if (!record) { 
+            this.deps.push(dep);
+            dep.pushWatch(this);
+        }
+    }　
+    clearDep () {
+        this.deps.forEach((i) => {
+            i.watchers = [];
+        })
+    }
+    get () {
+        pushStack(this);
+        this.render();
+        popStack();
+    }
+    update () {
+        this.render.call(this.ctx)
+    }
+}
+
 const obj = {name: 'zhou'};
-new Observer(obj);
+function mount () {
+   const input = document.getElementById('input');
+   const text = document.getElementById('text');
+   const render = () => { 
+        input.value = obj.name;
+        text.innerText = obj.name; 
+   }
+   const watcher = new Watcher(render);
+   new Observer(obj);   
+   input.value = obj.name;
+   input.addEventListener('input', (e)=> {
+        obj.name = e.target.value;
+   })
+}
 
-const view = document.getElementById('view');
-const content = document.getElementById('content');
-
-const render = (v) => {
-    view.value = v;
-    content.innerText = v;
-} 
-
-view.addEventListener("change", (e) => {
-   const v = e.target.value;
-   obj.name = v;
-})
-
-
-render(obj.name);
-
+mount();
